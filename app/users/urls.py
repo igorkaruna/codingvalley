3edcc38ee2847e1d665f9852450d16a01910cf50
Igor Karuna <ihor.karuna@gmail.com>
2023-01-28 20:18:11 +0200

    CV-9-add-register-endpoint

--- app/users/urls.py
+++ app/users/urls.py
@@ -8,6 +8,7 @@ from users.views import UserTokenObtainPairView
 
 urlpatterns = [
     path('', views.getRoutes),
+    path('register/', views.RegisterView.as_view(), name='register'),
     path('token/', UserTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
